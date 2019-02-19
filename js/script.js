179ca13304bce9ab0211576c110c11ea15399aa4
Dmytro <macho.cho98@gmail.com>
2019-02-19 20:26:18 +0200

    второе дз

--- js/script.js
+++ js/script.js
@@ -32,16 +32,91 @@ window.addEventListener('DOMContentLoaded', () => {
 
 			trigger.remove();
 
+			showConfirm();
+			calcGoods(1);
+
+
 			removeBtn.classList.add('goods__item-remove');
 			removeBtn.innerHTML = '&times';
 			item.appendChild(removeBtn);
 
 			cartWrapper.appendChild(item);
 			if (empty) {
-				empty.remove();
+				empty.style.display = 'none';
 			}		
+			calcTotal();
+
+			removeFromCart();
 		});
+
 	});
+
+	titles.forEach(function(item) {
+		if (item.textContent.length < 70) {
+			return;
+		} else {
+			const str = item.textContent.slice(0, 71) + '...';
+			
+			item.textContent = str;
+		}
+
+
+	});
+
+	function showConfirm() {
+		confirm.style.display = 'block';
+		let counter = 100;
+		const id = setInterval(frame, 10);
+
+		function frame() {
+				if (counter == 10) {
+					clearInterval(id);
+					confirm.style.display = 'none';
+				} else {
+
+				counter--;
+				confirm.style.opacity = '.' + counter;
+				confirm.style.transform = `translateY(-${counter}px)`;
+			}
+		}
+	}
+
+	function calcGoods(i) {
+		const items = cartWrapper.querySelectorAll('.goods__item'),
+		empty = cartWrapper.querySelector('.empty');;
+		badge.textContent = items.length + i;
+
+		if(items.length + i == 0) {
+					empty.style.display = 'block';
+		}
+	}
+
+	function calcTotal() {
+		const prices = document.querySelectorAll('.cart__wrapper > .goods__item > .goods__price > span');
+		let total = 0;
+		prices.forEach(function(item) {
+			total += +item.textContent;
+		});
+		totalCost.textContent = total;
+	}
+
+	function removeFromCart() {
+		const removeBtn = cartWrapper.querySelectorAll('.goods__item-remove');
+						
+		removeBtn.forEach(function(btn) {
+			btn.addEventListener('click', () => {
+			
+				btn.parentElement.remove();
+				calcGoods(0);
+
+				
+				calcTotal();
+				
+
+			})
+		});
+	}
+
 });
 
 
